05d1818e0f373c618fd7366c5a599509df2240a3
sdavidson140 <sdavidson140@gmail.com>
2019-04-06 21:21:37 -0400

    add logic to convert binary to text

--- lib/btd/converter.rb
+++ lib/btd/converter.rb
@@ -5,15 +5,21 @@ class BTD::Converter
 
   def convert(input)
     if @input_type == 'binary'
-      puts 'now a string'
+      binary_to_text(input)
     else
       text_to_binary(input)
     end
   end
 
+  def binary_to_text(binary)
+    binary.split(' ').each do |byte|
+      print byte.to_i(2).chr
+    end
+  end
+
   def text_to_binary(text)
     text.each_byte do |byte|
-      puts byte.to_s(2)
+      print "#{byte.to_s(2)} "
     end
   end
 end
